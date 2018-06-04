e3a2e80da5ccddc44fdfbebb6cb89eb9a772cd2f
Brandon Lam <brandonywlam@gmail.com>
2018-06-04 11:30:21 -0700

    fix content overflow

--- src/components/AverageWeek/index.js
+++ src/components/AverageWeek/index.js
@@ -3,8 +3,8 @@ import {Doughnut} from 'react-chartjs-2';
 import avg_week_data from './average_week_dataset';
 const AverageWeek = () => {
 	return(
-		<div style={{width:'330px', height:'330px'}}>
-			<Doughnut data={avg_week_data} width='400px' height='400px'/>
+		<div style={{width:'inherit', height:'inherit'}}>
+			<Doughnut data={avg_week_data} width='100%' height='100%'/>
 		</div>
 	)
 };
